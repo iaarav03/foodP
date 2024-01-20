3859b0d1d58861db1ade7aa0571f7efe9135afa0
iaarav03 <aarav.21ug2059@iiitranchi.ac.in>
2024-01-20 14:16:58 +0530

    first

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+    <h1>This page is in the process....</h1>
+    {/* <h4>This is your own company</h4> */}
+    </div>
+  )
+}
+
+export default About
